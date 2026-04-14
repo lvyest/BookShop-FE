f1e14e93310ade476428189881e16aa1f5c11a96
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-04-14 23:34:32 +0900

    feat: 제목 컴포넌트 렌더링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Title from "../components/common/Title";
+
 function home() {
     return (
         <>
+        <Title size="medium" color="background">
+            제목 테스트
+        </Title>
         <div>Home</div>
         </>
     )
