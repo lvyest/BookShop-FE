f4183f566f148b43c0ce8e90afe06f986a89cc2c
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-04-19 16:14:10 +0900

    feat: Error페이지 Layout

--- src/components/common/Error.tsx
+++ src/components/common/Error.tsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import Layout from "../layout/Layout";
 
 interface RouteError {
     statusText?: string;
@@ -7,11 +8,11 @@ interface RouteError {
 function Error() {
     const error = useRouteError() as RouteError;
     return (
-        <div>
+        <Layout>
             <h1>오류가 발생했습니다.</h1>
             <p>다음과 같은 오류가 발생했습니다. </p>
             <p>{error.statusText || error.message}</p>
-        </div>
+        </Layout>
     )
 }
 
