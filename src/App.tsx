d322d7fb40023731a1c7bf8564de2f1c51cb563f
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-04-21 04:09:40 +0900

    feat: 로그인 Zustand로 구현 및 헤더 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./components/common/Error";
 import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassword";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/reset",
     element: <Layout><ResetPassword/></Layout>
+  },
+    {
+    path: "/login",
+    element: <Layout><Login/></Layout>
   }
 ])
 
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,10 +3,12 @@ import logo from '../../assets/images/logo.png';
 import { FaSignInAlt, FaRegUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useCategory } from "../../hooks/useCategory";
+import { useAuthStore } from "../../store/authStore";
 
 
 function Header() {
     const { category } = useCategory();
+    const { isloggedIn, storeLogout} = useAuthStore();
 
     return (
         <HeaderStyle>
@@ -26,18 +28,31 @@ function Header() {
                 </ul>
             </nav>
             <nav className="auth">
-                <ul>
-                    <li>
-                        <a href="/login">
-                            <FaSignInAlt />로그인
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/signup">
-                            <FaRegUser />회원가입
-                        </a>
-                    </li>
-                </ul>
+                {
+                    isloggedIn && (
+                        <ul>
+                            <li><Link to="/cart">장바구니</Link></li>
+                            <li><Link to="/orderlist">주문 내역</Link></li>
+                            <li><button onClick={storeLogout}>로그아웃</button></li>
+                        </ul>
+                    )
+                }
+                {
+                    !isloggedIn && (
+                        <ul>
+                            <li>
+                                <a href="/login">
+                                    <FaSignInAlt />로그인
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/signup">
+                                    <FaRegUser />회원가입
+                                </a>
+                            </li>
+                        </ul>
+                    )
+                }
             </nav>           
         </HeaderStyle>
     ) 
@@ -84,14 +99,16 @@ const HeaderStyle = styled.header`
             display: flex;
             gap: 16px;
             li {
-                a {
+                a, button {
                     font-size: 1rem;
                     font-weight: 600;
                     text-decoration: none;
                     display: flex;
                     align-item: center;
                     line-height: 1;
-
+                    background: none;
+                    border: 0;
+                    cursor: pointer;
                     svg {
                         margin-right: 6px;
                     }
